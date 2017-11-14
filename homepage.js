0b2a0db7c56c30f0ea2791d29cd71644c8ea2f34
npadrid <npadrid@uci.edu>
2017-11-13 23:33:39 -0800

    added discover page

--- homepage.js
+++ homepage.js
@@ -28,31 +28,43 @@ $('#projects-footer').click(function() {
         'slow');
 });
 
+$(function (){
+  var i, allPics, inPic, outPic;
+  for (i = 1, allPics = $('.pics').length, i < allPics; i++) {
+    inPic = "#pic" + i;
+    if(i == 1){
+      console.log("first pic");
+      $('#pic1').delay(1000).fadeOut(500);
+    }
+    else{
+      $(fadingPic).fadeIn(500).delay(1000).fadeOut(500);
+    }
+});
 // Javascript for timeline by Sebastiano Guerriero from CodyHouse
 
-jQuery(document).ready(function($){
-	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
-
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
-
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame)
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
-	});
-
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-		});
-	}
-
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
-});
+// jQuery(document).ready(function($){
+// 	var timelineBlocks = $('.cd-timeline-block'),
+// 		offset = 0.8;
+//
+// 	//hide timeline blocks which are outside the viewport
+// 	hideBlocks(timelineBlocks, offset);
+//
+// 	//on scolling, show/animate timeline blocks when enter the viewport
+// 	$(window).on('scroll', function(){
+// 		(!window.requestAnimationFrame)
+// 			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+// 			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+// 	});
+//
+// 	function hideBlocks(blocks, offset) {
+// 		blocks.each(function(){
+// 			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+// 		});
+// 	}
+//
+// 	function showBlocks(blocks, offset) {
+// 		blocks.each(function(){
+// 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+// 		});
+// 	}
+// });
