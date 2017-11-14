080ce74d1b51d75ebada729921a2453b508e9b05
npadrid <npadrid@uci.edu>
2017-11-13 18:36:48 -0800

    fixed footer links

--- homepage.js
+++ homepage.js
@@ -15,3 +15,15 @@ $('#discover-link').click(function() {
         scrollTop: $(".content-discover").offset().top},
         'slow');
 });
+
+$('#about-footer').click(function() {
+    $('html,body').animate({
+        scrollTop: $(".content-about").offset().top},
+        'slow');
+});
+
+$('#projects-footer').click(function() {
+    $('html,body').animate({
+        scrollTop: $(".content-projects").offset().top},
+        'slow');
+});
